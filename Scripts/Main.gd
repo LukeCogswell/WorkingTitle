f193bcec5edf9050dbfd24d4db1a5af7058f2d4c
Tim Gatto <timjgatto@gmail.com>
2023-01-03 17:34:18 -0500

    added script to fill screen with castles

---
+++ Scripts/Main.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var castle = preload("res://Scenes/Castle.tscn")
+
+# very basic function that fills the screen with castles
+func make_tiles(cols : int = 32, rows : int = 18):
+	for x in cols:
+		for y in rows:
+			var new_pos = Vector2(x * 8, y * 8)
+			var new_castle = castle.instance()
+			new_castle.position = new_pos
+			add_child(new_castle)
+
+func _ready():
+	make_tiles()
