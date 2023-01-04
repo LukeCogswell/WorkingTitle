a2fc3f051e724d6f44fd5b49942ba931f657f667
Tim Gatto <timjgatto@gmail.com>
2023-01-04 09:57:17 -0500

    basic noise generation

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -7,6 +7,12 @@ var mountain = preload("res://Scenes/Mountain.tscn")
 
 var tiles = [castle, house, grass, mountain]
 
+onready var noise = $"land noise".texture.noise
+
+func _ready():
+	generate_using_noise()
+#	make_tiles()
+
 # very basic function that fills the screen with castles
 # default values based on screen height/width
 func make_tiles(cols : int = 32, rows : int = 18):
@@ -18,5 +24,17 @@ func make_tiles(cols : int = 32, rows : int = 18):
 			new_castle.position = new_pos
 			add_child(new_castle)
 
-func _ready():
-	make_tiles()
+func generate_using_noise(cols : int = 32, rows : int = 18):
+	for x in cols:
+		for y in rows:
+			var pos = Vector2(x * 8, y * 8)
+			var val = noise.get_noise_2d(pos.x, pos.y)
+			if val > 0:
+				make_at(grass, pos)
+			else:
+				make_at(mountain, pos)
+
+func make_at(scene, pos : Vector2):
+	var new_child = scene.instance()
+	new_child.position = pos
+	add_child(new_child)
