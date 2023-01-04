5854d8d508f74e9004d2c7d6ed9f8db23a3b05fb
Tim Gatto <timjgatto@gmail.com>
2023-01-03 21:35:01 -0500

    Added house scene

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 var castle = preload("res://Scenes/Castle.tscn")
+var house = preload("res://Scenes/House.tscn")
+
+var tiles = [castle, house]
 
 # very basic function that fills the screen with castles
 # default values based on screen height/width
@@ -8,7 +11,8 @@ func make_tiles(cols : int = 32, rows : int = 18):
 	for x in cols:
 		for y in rows:
 			var new_pos = Vector2(x * 8, y * 8)
-			var new_castle = castle.instance()
+			var choice = rand_range(0, len(tiles))
+			var new_castle = tiles[choice].instance()
 			new_castle.position = new_pos
 			add_child(new_castle)
 
